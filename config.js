740c9299a36e3ea0b88ea2b8de9daf276e5f65e6
phh02356-a11y <phh02356@gmail.com>
2026-04-02 19:50:51 +0800

    Modify PROJECT_DATA in config.js
    Updated symbol and added gbLink to PROJECT_DATA.

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const PROJECT_DATA = {
     name: "小丁",
-    symbol: "https://github.com/phh02356-a11y/Meme-TRUMP-1",
-    ca: "tap", 
-    pumpLink: "https://pump.fun" 
+    symbol: "爱嫖娼", 
+    ca: "tap",
+    pumpLink: "https://pump.fun",
+    gbLink: "https://github.com/phh02356-a11y/Meme-TRUMP-1" // 这是新加的一项
 };
-
