17c01597232b3c30eb95bd13d9a0d0cb0bf0b7d0
phh02356-a11y <phh02356@gmail.com>
2026-04-02 18:29:58 +0800

    Clean up config.js by removing unused data
    Removed project data and commemorative message from config.js

--- config.js
+++ config.js
@@ -4,13 +4,4 @@ const PROJECT_DATA = {
     ca: "我的代币地址", 
     pumpLink: "https://pump.fun" 
 };
-<script>
-    const PROJECT_DATA = {
-        name: "TRUMP: LIBERATION DAY", 
-        symbol: "$TRUMP",
-        ca: "TBA (Wait for the Storm...)", 
-        pumpLink: "https://pump.fun" 
-    };
-</script>
 
-<p style="color: #ffd700; font-style: italic;">Commemorating the 1-Year Anniversary of the Economic Revolution.</p>
